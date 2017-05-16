f38a783f356f0080df7f3db2a1760e643ed7457f
nomi-github <nomindalain@gmail.com>
2017-05-16 15:36:33 +0800

    Update currency_app.py

--- currency_app.py
+++ currency_app.py
@@ -42,54 +42,6 @@ def processRequest(req):
     if req.get("result").get("action") != "tdbCurrencyConverter":
         return {}
     baseurl = "http://www.tdbm.mn/script.php?mod=rate&ln=mn"
-    #result = req.get("result")
-	#parameters = result.get("parameters")
-    #currency = parameters.get("currency")
-    #if city is None:
-    #    return None#!/usr/bin/env python
-
-from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
-
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-
-import json
-import os
-
-from flask import Flask
-from flask import request
-from flask import make_response
-
-from bs4 import BeautifulSoup
-
-						 
-# Flask app should start in global layout
-app = Flask(__name__)
-
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
-
-    print("Request:")
-    print(json.dumps(req, indent=4))
-
-    res = processRequest(req)
-
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-
-def processRequest(req):
-    if req.get("result").get("action") != "tdbCurrencyConverter":
-        return {}
-    baseurl = "http://www.tdbm.mn/script.php?mod=rate&ln=mn"
     result = req.get("result")
 	parameters = result.get("parameters")
     currency = parameters.get("currency")
@@ -109,13 +61,13 @@ def processRequest(req):
 
 
 def makeWebhookResult(data, valutName):
-	speech = "not found"
+	speech = ""
 
 	for value in data:
 		key=''.join(value[0].split())
-		if (len(value)==6 && (key==valutName):
+		if (len(value)==6 && (key==valutName or key=="all") ):
 			#print(value[4])
-			speech = "Onoodriin " + key + "-n hansh: " + data[0][1] + ", " + value[1]  + ", " + \
+			speech += "\nOnoodriin " + key + "-n hansh: " + data[0][1] + ", " + value[1]  + ", " + \
 				 data[1][0] + ", " + data[2][0] + ", " + value[2] + \
 				 data[1][0] + ", " + data[2][1] + ", " + value[3] + \
 				 data[1][1] + ", " + data[2][0] + ", " + value[4] + \
@@ -126,38 +78,6 @@ def makeWebhookResult(data, valutName):
     return {
         "speech": speech,
         "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-nomi-test-currency-converter-webhook-sample"
-    }
-
-
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-
-    print("Starting app on port %d" % port)
-
-    app.run(debug=False, port=port, host='0.0.0.0')
-
-
-		
-    #yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(baseurl).read()
-    table_data = [[cell.text for cell in row("td")]
-                         for row in BeautifulSoup(result)("tr")]
-	data = json.dumps(OrderedDict(table_data))
-	#data = json.loads(result)
-    print data
-	#res = makeWebhookResult(data)
-    return res
-
-
-
-def makeWebhookResult(data):
-
-    return {
-        "speech": "here is your result",
-        "displayText": data,
         # "data": data,
         # "contextOut": [],
         "source": "apiai-nomi-test-currency-converter-webhook-sample"
