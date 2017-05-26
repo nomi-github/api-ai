c5296708936335a4105651d4bffd3331105f5c3c
nomi-github <nomindalain@gmail.com>
2017-05-26 14:13:45 +0800

    Update currency_app.py

--- currency_app.py
+++ currency_app.py
@@ -110,7 +110,7 @@ def makeWebhookResult(data, valutName):
 						{
 						    "title": data[1][0],
 						    "image_url": "https://seeklogo.com/images/T/TDB-logo-EE3C11F918-seeklogo.com.gif",
-						    "subtitle": data[2][0] + ": " + value[2] + ", " + value[3],
+						    "subtitle": data[2][0] + ": " + value[2] + ", " +  data[2][1] + ": " value[3],
 						    "default_action": {
 							"type": "web_url",
 							"url": "http://tdbm.mn/mn/exchange",
