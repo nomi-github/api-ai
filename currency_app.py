ddb05b6de3cd0081ee4bf7e0ee8c9d675320ed68
nomi-github <nomindalain@gmail.com>
2017-05-26 12:20:43 +0800

    Update currency_app.py

--- currency_app.py
+++ currency_app.py
@@ -89,58 +89,62 @@ def makeWebhookResult(data, valutName):
 				 data[1][1] + "-" + data[2][1] + ": " + value[5]
 	 		facebookData={
 				"facebook": {
-				    "attachment": {
-				      "type": "template",
-				      "payload": {
-					"template_type": "generic",
-					"elements": [
-					  {
-					    "title": "Smurfs: The Lost Village (2017)",
-					    "image_url": "https://www.moovrika.com/ext/makeimg.php?tbl=movies&id=15666&img=1&type=image&movie=Smurfs+The+Lost+Village&fs=400",
-					    "subtitle": "Smurfette attempts to find her purpose in the village. When she encounters a creature in the Forbidden Forest who drops a mysterious map, she sets off with her friends Brainy, Clumsy, and Hefty on an adventure to find the Lost Village before the evil wizard Gargamel does.",
-					    "default_action": {
-					      "type": "web_url",
-					      "url": "https://www.moovrika.com/m/15666",
-					      "webview_height_ratio": "tall"
-					    },
-					    "buttons": [
-					      {
-						"title": "more info",
-						"type": "web_url",
-						"url": "https://www.moovrika.com/m/4082",
-						"webview_height_ratio": "tall"
-					      },
-					      {
-						"title": "View trailer",
-						"type": "web_url",
-						"url": "https://www.moovrika.com/m/4082",
-						"webview_height_ratio": "tall"
-					      }
-					    ]
+					"text": {
+						"Onoodriin " + key + "-n hansh: " + data[0][1] + ": " + value[1]
+					},
+					"attachment": {
+					  	"type": "template",
+					  	"payload": {
+						"template_type": "generic",
+						"elements": [
+					  	{
+							"title": data[1][0],
+							"image_url": "https://seeklogo.com/images/T/TDB-logo-EE3C11F918-seeklogo.com.gif",
+							"subtitle": data[2][0] + ": " + value[2] + ", " + data[2][1] + ": " + value[3],
+							"default_action": {
+							  "type": "web_url",
+							  "url": "https://www.moovrika.com/m/15666",
+							  "webview_height_ratio": "tall"
+						},
+						"buttons": [
+						  {
+							"title": "Avah",
+							"type": "web_url",
+							"url": "https://www.moovrika.com/m/4082",
+							"webview_height_ratio": "tall"
+						  },
+						  {
+							"title": "Zarah",
+							"type": "web_url",
+							"url": "https://www.moovrika.com/m/4082",
+							"webview_height_ratio": "tall"
+						  }
+						]
 					  },
 					  {
-					    "title": "Resident Evil: The Final Chapter (2017)",
-					    "image_url": "https://www.moovrika.com/ext/makeimg.php?tbl=movies&id=4167&img=1&type=image&movie=Resident+Evil+The+Final+Chapter&fs=400",
-					    "subtitle": "Resident Evil: The Final Chapter is an upcoming science fiction action horror film written and directed by Paul W. S. Anderson. It is the sequel to Resident Evil: Retribution (2012), and will be the sixth and final installment in the Resident Evil film series, which is very loosely based on the Capcom survival horror video game series Resident Evil.",
-					    "default_action": {
-					      "type": "web_url",
-					      "url": "https://www.moovrika.com/m/4167",
-					      "webview_height_ratio": "tall"
-					    },
-					    "buttons": [
-					      {
-						"title": "more info",
+						"title": data[1][1],
+						"image_url": "https://seeklogo.com/images/T/TDB-logo-EE3C11F918-seeklogo.com.gif",
+						"subtitle": data[2][0] + ": " + value[4] + ", " + data[2][1] + ": " + value[5]
+						"default_action": {
+						  "type": "web_url",
+						  "url": "https://www.moovrika.com/m/4167",
+						  "webview_height_ratio": "tall"
+						},
+						"buttons": [
+						  {
+						"title": "Salbaruudiig harah",
 						"type": "web_url",
 						"url": "https://www.moovrika.com/m/4082",
 						"webview_height_ratio": "tall"
-					      }
-					    ]
+						  }
+						]
 					  }
 					]
-				      }
-				    }
-				  }
+					  }
+					}
+				}
 			}
+			
 			#print("%s: %s, %s: %s, %s-%s: %s, %s-%s: %s, %s-%s: %s, %s-%s: %s," %(data[0][0], key, data[0][1], value[1], data[1][0],data[2][0],value[2],data[1][0],data[2][1],value[3],data[1][1],data[2][0],value[4],data[1][1],data[2][1],value[5]))
 
 	return {
