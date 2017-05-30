1382d70d309c2c3475cc1b2b6e79f83bd65d13ea
nomi-github <nomindalain@gmail.com>
2017-05-30 11:42:47 +0800

    Update mainWebhook.py

--- mainWebhook.py
+++ mainWebhook.py
@@ -77,8 +77,8 @@ def processSpecificDistrictBranch(parameters):
 		#phone = xmlValue.getElementsByTagName('PHONE')
 		#timeCode = xmlValue.getElementsByTagName('TIMECODE')
 		#print (branchDistCode + ', ' + tittle)
-	#	if (branchDistCode == distCode):
-		speech += branchDistCode + ": " + tittle
+		if (branchDistCode == distCode):
+			speech += branchDistCode + ": " + tittle
 	
 			#print("%s: %s, %s: %s, %s-%s: %s, %s-%s: %s, %s-%s: %s, %s-%s: %s," %(data[0][0], key, data[0][1], value[1], data[1][0],data[2][0],value[2],data[1][0],data[2][1],value[3],data[1][1],data[2][0],value[4],data[1][1],data[2][1],value[5]))
 
